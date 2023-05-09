0d738804c4d4f4803ed5785a38cc1668a38f1aac
Daniel_Dyn <102476758+DanieL-S22@users.noreply.github.com>
2023-05-09 00:05:08 -0300

    rotas criadas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/cadastro-usuario', function () {
     return view('welcome');
+});
+
+Route::get('/login', function(){
+
+});
+
+Route::get('/home', function(){
+
+});
+
+Route::get('cadastro-ave', function(){
+
 });
