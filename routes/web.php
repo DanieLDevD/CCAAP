83f87a0a2ccb2e1168b3123c449e7cfafb50a9e5
Daniel_Dyn <102476758+DanieL-S22@users.noreply.github.com>
2023-05-09 23:38:02 -0300

    controllers user e models configurado, BD ok

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\{
+    UserController
+};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/cadastro-usuario', function () {
-    return view('cadastro');
-});
+Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 
 Route::get('/login', function(){
 
