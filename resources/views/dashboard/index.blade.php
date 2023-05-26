e60fe731ec941c9913305706b2e02e53f08fbb42
Daniel_Dyn <102476758+DanieL-S22@users.noreply.github.com>
2023-05-25 21:38:55 -0300

    making buttom leave in dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -21,10 +21,11 @@
                         <input type="text" placeholder="Pesquisar Ave" class="px-4 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
                         <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg ml-2">Pesquisar</button>
                     </form>
+                    <a href="{{ route('login') }}" class="text-gray-300 ml-4 inline-block border border-green-300 px-4 py-2 rounded-lg hover:bg-green-300 hover:text-white">Sair</a>
                 </div>
             </div>
         </nav>
-        
+
         <!-- Body -->
         <div class="container mx-auto mt-8">
             <h2 class="text-2xl font-bold mb-4">Lista de Aves Cadastradas</h2>
