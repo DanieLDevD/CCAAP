f26390dda92a6814a485f4ee43af369c0c893ad9
Daniel_Dyn <102476758+DanieL-S22@users.noreply.github.com>
2023-05-27 17:59:01 -0300

    edittin front-end colors and text in tables

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -9,11 +9,11 @@
 <body>
     <div class="flex flex-col min-h-screen">
         <!-- Navbar -->
-        <nav class="bg-gray-800 text-white py-4 px-6">
+        <nav class="bg-green-800 text-white py-4 px-6">
             <div class="container mx-auto flex justify-between items-center">
                 <div>
-                    <a href="#" class="text-xl font-bold">Logo</a>
-                    <h3 class="text-2xl font-bold mb-4">Seja Bem Vindo, <span class="text-blue-500 hover:text-blue-700">{{ $compoundName }}</span>, ao CCAAP! </h2>
+                    <a href="#" class="text-xl font-bold text-green-200">Logo</a>
+                    <h3 class="text-2xl font-bold mb-4">Seja Bem Vindo, <span class="text-blue-500 hover:text-blue-700">{{ $compoundName }}</span>, ao CCAAP!</h2>
                 </div>
                 <div>
                     <a href="{{ route('birds.create') }}" class="text-gray-300 mr-4">Cadastrar Ave</a>
@@ -30,20 +30,34 @@
         <!-- Body -->
         <div class="container mx-auto mt-8">
             <h2 class="text-2xl font-bold mb-4">Lista de Aves Cadastradas</h2>
-            <ul class="border border-gray-200 rounded-lg">
-                @foreach($birds as $bird)
-                <li class="px-4 py-2">
-                    <strong>Nome:</strong> {{ $bird->nome }}<br>
-                    <strong>Espécie:</strong> {{ $bird->especie }}<br>
-                    <strong>Anilha:</strong> {{ $bird->anilha }}<br>
-                    <strong>Anilha Legal:</strong> {{ $bird->anilha_legal }}<br>
-                    <strong>Data de Nascimento:</strong> {{ $bird->data_nascimento }}<br>
-                    <strong>Sexo:</strong> {{ $bird->sexo }}<br>
-                    <strong>Mãe:</strong> {{ $bird->mae }}<br>
-                    <strong>Pai:</strong> {{ $bird->pai }}
-                </li>
-                @endforeach
-            </ul>
+            <table class="min-w-full border border-gray-200 rounded-lg">
+                <thead>
+                    <tr>
+                        <th class="px-4 py-2 bg-green-200 text-green-800 text-center">Nome</th>
+                        <th class="px-4 py-2 bg-green-200 text-green-800 text-center">Espécie</th>
+                        <th class="px-4 py-2 bg-green-200 text-green-800 text-center">Anilha</th>
+                        <th class="px-4 py-2 bg-green-200 text-green-800 text-center">Anilha Legal</th>
+                        <th class="px-4 py-2 bg-green-200 text-green-800 text-center">Data de Nascimento</th>
+                        <th class="px-4 py-2 bg-green-200 text-green-800 text-center">Sexo</th>
+                        <th class="px-4 py-2 bg-green-200 text-green-800 text-center">Mãe</th>
+                        <th class="px-4 py-2 bg-green-200 text-green-800 text-center">Pai</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($birds as $bird)
+                    <tr>
+                        <td class="px-4 py-2 text-center">{{ $bird->nome }}</td>
+                        <td class="px-4 py-2 text-center">{{ $bird->especie }}</td>
+                        <td class="px-4 py-2 text-center">{{ $bird->anilha }}</td>
+                        <td class="px-4 py-2 text-center">{{ $bird->anilha_legal }}</td>
+                        <td class="px-4 py-2 text-center">{{ $bird->data_nascimento }}</td>
+                        <td class="px-4 py-2 text-center">{{ $bird->sexo }}</td>
+                        <td class="px-4 py-2 text-center">{{ $bird->mae }}</td>
+                        <td class="px-4 py-2 text-center">{{ $bird->pai }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </div>
 
