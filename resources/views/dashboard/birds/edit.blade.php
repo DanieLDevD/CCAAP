a96e2571dbbec5f343bfde1203d6e1963f326b3b
Victor-Prudente <victoreduprudente@gmail.com>
2023-06-02 23:58:27 -0400

    Revisando tela de editar

--- resources/views/dashboard/birds/edit.blade.php
+++ resources/views/dashboard/birds/edit.blade.php
@@ -9,23 +9,23 @@
             @method('PUT')
             <div class="mb-4">
                 <label for="nome" class="block text-gray-700 font-bold mb-2">Nome:</label>
-                <input type="text" id="nome" name="nome" value="{{ $bird->nome }}" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                <input type="text" id="nome" name="nome" value="{{ $bird->nome }}" class="w-full px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500">
             </div>
             <div class="mb-4">
                 <label for="especie" class="block text-gray-700 font-bold mb-2">Espécie:</label>
-                <input type="text" id="especie" name="especie" value="{{ $bird->especie }}" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                <input type="text" id="especie" name="especie" value="{{ $bird->especie }}" class="w-full px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500">
             </div>
             <div class="mb-4">
                 <label for="anilha" class="block text-gray-700 font-bold mb-2">Anilha:</label>
-                <input type="text" id="anilha" name="anilha" value="{{ $bird->anilha }}" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                <input type="text" id="anilha" name="anilha" value="{{ $bird->anilha }}" class="w-full px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500">
             </div>
             <div class="mb-4">
                 <label for="anilha_legal" class="block text-gray-700 font-bold mb-2">Anilha Legal:</label>
-                <input type="text" id="anilha_legal" name="anilha_legal" value="{{ $bird->anilha_legal }}" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                <input type="text" id="anilha_legal" name="anilha_legal" value="{{ $bird->anilha_legal }}" class="w-full px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500">
             </div>
             <div class="mb-4">
                 <label for="data_nascimento" class="block text-gray-700 font-bold mb-2">Data de Nascimento:</label>
-                <input type="date" id="data_nascimento" name="data_nascimento" value="{{ date('d/m/Y', strtotime($bird->data_nascimento)) }}" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                <input type="date" id="data_nascimento" name="data_nascimento" value="{{ $bird->data_nascimento }}" class="w-full px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500">
             </div>
             <div class="mb-4">
                 <label for="sexo" class="block text-gray-700 font-bold mb-2">Sexo:</label>
@@ -37,11 +37,11 @@
             </div>
             <div class="mb-4">
                 <label for="mae" class="block text-gray-700 font-bold mb-2">Mãe:</label>
-                <input type="text" id="mae" name="mae" value="{{ $bird->mae }}" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                <input type="text" id="mae" name="mae" value="{{ $bird->mae }}" class="w-full px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500">
             </div>
             <div class="mb-4">
                 <label for="pai" class="block text-gray-700 font-bold mb-2">Pai:</label>
-                <input type="text" id="pai" name="pai" value="{{ $bird->pai }}" class="px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500">
+                <input type="text" id="pai" name="pai" value="{{ $bird->pai }}" class="w-full px-3 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500">
             </div>
             <div class="flex justify-between items-center mb-4">
                 <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">Atualizar</button>
