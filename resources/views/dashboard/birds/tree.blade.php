6c369277495f9cf4901148c705de41fa73576fc7
JaquelineBravin <111389555+JaquelineBravin@users.noreply.github.com>
2023-06-08 22:39:54 -0400

    criando front da arvore genealogica

--- resources/views/dashboard/birds/tree.blade.php
+++ resources/views/dashboard/birds/tree.blade.php
@@ -1,4 +1,6 @@
 @extends('layouts.app')
+@section('content')
+
 <!-- Navbar -->
 <nav class="bg-green-800 py-4 px-6">
     <div class="container mx-auto flex justify-between items-center">
@@ -10,3 +12,27 @@
         </div>
     </div>
 </nav>
+
+<div class="container">
+    <div class="py-4 px-4">
+        <table>
+            <tbody>
+                <tr class="flex flex-col h-10 bg-slate-700">
+                    <td class="px-4 py-2 text-center">Nome:</td>
+                    <td class="px-4 py-2 text-center">Sexo:</td>
+                    <td class="px-4 py-2 text-center">Avó:</td>
+                    <td class="px-4 py-2 text-center">Avô:</td>
+                    <td class="px-4 py-2 text-center">Mãe:</td>
+                    <td class="px-4 py-2 text-center">Pai:</td>
+                    <td class="px-4 py-2 text-center">Irmãos:</td>
+                    <td class="px-4 py-2 text-center">Conjugue:</td>
+                    <td class="px-4 py-2 text-center">Filhotes:</td>
+                    <td class="px-4 py-2 text-center">
+                        <a href="" class="text-blue-500 hover:text-blue-700">Editar</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>        
+    </div>
+</div>
+@endsection
