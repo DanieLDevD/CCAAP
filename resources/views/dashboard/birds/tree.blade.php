d2a7cd4e81ac46797fef93769587b8ad41b146e6
Daniel_Dyn <102476758+DanieL-S22@users.noreply.github.com>
2023-06-10 00:03:34 -0300

    Made best Ui design

--- resources/views/dashboard/birds/tree.blade.php
+++ resources/views/dashboard/birds/tree.blade.php
@@ -12,33 +12,37 @@
         </div>
     </div>
 </nav>
-
-<div class="container">
 <div class="container mx-auto py-4 px-4">
-        <table class="w-full bg-white shadow-md rounded">
-            <thead>
-                <tr class="bg-gray-200">
-                    <th class="py-2 px-4 text-center">Ave</th>
-                    <th class="py-2 px-4 text-center">Sexo</th>
-                    <th class="py-2 px-4 text-center">Mãe</th>
-                    <th class="py-2 px-4 text-center">Pai</th>
-                    <th class="py-2 px-4 text-center"></th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($birds as $bird)
-                <tr>
-                    <td class="py-2 px-4 text-center">{{ $bird->nome }}</td>
-                    <td class="py-2 px-4 text-center">{{ $bird->sexo }}</td>
-                    <td class="py-2 px-4 text-center">{{ $bird->mae }}</td>
-                    <td class="py-2 px-4 text-center">{{ $bird->pai }}</td>
-                    <td class="py-2 px-4 text-center">
-                        <!-- Botões ou ações adicionais aqui -->
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+    <div class="flex items-center justify-center">
+        <div class="w-10/12 mx-auto">
+            <div class="relative">
+                <table class="w-full bg-white shadow-md rounded">
+                    <thead>
+                        <tr class="bg-gray-200">
+                            <th class="py-2 px-4 text-center">Ave</th>
+                            <th class="py-2 px-4 text-center">Sexo</th>
+                            <th class="py-2 px-4 text-center">Mãe</th>
+                            <th class="py-2 px-4 text-center">Pai</th>
+                            <th class="py-2 px-4 text-center"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($birds as $bird)
+                        <tr>
+                            <td class="py-2 px-4 text-center">{{ $bird->nome }}</td>
+                            <td class="py-2 px-4 text-center">{{ $bird->sexo }}</td>
+                            <td class="py-2 px-4 text-center">{{ $bird->mae }}</td>
+                            <td class="py-2 px-4 text-center">{{ $bird->pai }}</td>
+                            <td class="py-2 px-4 text-center">
+                                <!-- Botões ou ações adicionais aqui -->
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
+
 @endsection
